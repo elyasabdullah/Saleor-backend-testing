5a6c5e4c40811a75f4d69ca15bd092c734543ae3
Elyas Abdullah <99258943+elyasabdullah@users.noreply.github.com>
2026-02-03 09:06:19 +0300

    fix build

--- src/graphql/fragments/VendorFields/__generated__/VendorFields.tsx
+++ src/graphql/fragments/VendorFields/__generated__/VendorFields.tsx
@@ -1,24 +1,37 @@
 /* eslint-disable */
 /* prettier-ignore */
-import * as Types from '../../../types';
 
 import { gql } from '@apollo/client';
-export type VendorFieldsFragment = { id: string, name: string, description?: string | null, logo: string, isActive: boolean, createdAt: any, updatedAt: any, owner?: { id: string, email: string, firstName: string, lastName: string } | null };
+export type VendorFieldsFragment = {
+  id: string;
+  name: string;
+  description?: string | null;
+  logo: string;
+  isActive: boolean;
+  createdAt: any;
+  updatedAt: any;
+  owner?: {
+    id: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+  } | null;
+};
 
 export const VendorFieldsFragmentDoc = gql`
-    fragment VendorFields on Vendor {
-  id
-  name
-  description
-  logo
-  isActive
-  owner {
+  fragment VendorFields on Vendor {
     id
-    email
-    firstName
-    lastName
+    name
+    description
+    logo
+    isActive
+    owner {
+      id
+      email
+      firstName
+      lastName
+    }
+    createdAt
+    updatedAt
   }
-  createdAt
-  updatedAt
-}
-    `;
+`;
